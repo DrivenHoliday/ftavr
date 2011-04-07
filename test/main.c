fcbaa6a1e14dcdd812efe9ec82f9444af63d9a51
kaiser <kaiser@a5b627e9-ff9b-4b74-a2bc-6bb990413350>
2011-04-07 14:58:59 +0000

    Schaltpläne überarbeitet
    git-svn-id: file:///home/florian/sftp-avr/trunk@9 a5b627e9-ff9b-4b74-a2bc-6bb990413350

--- test/main.c
+++ test/main.c
@@ -4,175 +4,20 @@
 
 #include <avr/delay.h>
 
-#define BAUD 19200
-#include <util/setbaud.h>
-
-#include "uart.h"
-
 #include <stdint.h>
+
+#define FOREVER for(;;)
 
 int main(void)
 {
-    uint8_t last[2] = {0,0};
-
-    uart_init(UBRR_VALUE);
-
-    DDRD = 0x0;
-    PORTD = 0x0;
-
     DDRC = 0xff;
-    PORTC = 0x0;
+    PORTC = 0xff;
     
-    sei();
-
-    while(1)
+    FOREVER
     {
-        if(PIND & (1<<PC2))
-        {
-            if(!last[0])
-            {
-                uart_buf_puts("Tor 1\n");
-                last[0] = 1;
-            }
-        }
-        else
-            last[0] = 0;
-
-        if(PIND & (1<<PC3))
-        {
-            if(!last[1])
-            {
-                uart_buf_puts("Tor 2\n");
-                last[1] = 1;
-            }
-        }
-        else
-            last[1] = 0;
-
-        if(last[0] || last[1])
-            PORTC = 0x0;
-        else
-            PORTC = 0xff;
-
+        _delay_ms(1000);
+        PORTC = ~PORTC;
     }
-
-/*
-uint8_t chgd[5] = {0,0,0,0,0};
-    uart_init(UBRR_VALUE);
-
-    sei();
-
-    while(1)
-    {
-        if(PINB & (1<<PC0))
-        {
-            if(!chgd[0])
-            {
-                uart_buf_puts("Taster 1 an\n");
-                chgd[0] = 1;
-            }
-        }
-        else
-        {
-            if(chgd[0])
-            {
-                uart_buf_puts("Taster 1 aus\n");
-                chgd[0] = 0;
-            }
-        }
-
-        if(PINB & (1<<PC1))
-        {
-            if(!chgd[1])
-            {
-                uart_buf_puts("Taster 2 an\n");
-                chgd[1] = 1;
-            }
-        }
-        else
-        {
-            if(chgd[1])
-            {
-                uart_buf_puts("Taster 2 aus\n");
-                chgd[1] = 0;
-            }
-        }
-
-        if(PINB & (1<<PC2))
-        {
-            if(!chgd[2])
-            {
-                uart_buf_puts("Taster 3 an\n");
-                chgd[2] = 1;
-            }
-        }
-        else
-        {
-            if(chgd[2])
-            {
-                uart_buf_puts("Taster 3 aus\n");
-                chgd[2] = 0;
-            }
-        }
-
-
-        if(PINB & (1<<PC3))
-        {
-            if(!chgd[3])
-            {
-                uart_buf_puts("Taster 4 an\n");
-                chgd[3] = 1;
-            }
-        }
-        else
-        {
-            if(chgd[3])
-            {
-                uart_buf_puts("Taster 4 aus\n");
-                chgd[3] = 0;
-            }
-        }
-
-
-        if(PINB & (1<<PC4))
-        {
-            if(!chgd[4])
-            {
-                uart_buf_puts("Taster 5 an\n");
-                chgd[4] = 1;
-            }
-        }
-        else
-        {
-            if(chgd[4])
-            {
-                uart_buf_puts("Taster 5 aus\n");
-                chgd[4] = 0;
-            }
-        }
-
-    }
-*/
-
-/*
-DDRC = 0xff;
-PORTC = 0x0;
-
- uart_init(UBRR_VALUE);
-while(1)
-{
-_delay_ms(1000);
-PORTC = ~PORTC;
-uart_putc('x'); 
-}
-
-*/
-/*
-
-while(1)
-    {
-}
-*/
 
     /* wird nie erreicht */
     return 0;
